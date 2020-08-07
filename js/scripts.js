41a4f49e7e528dd1d8b89c242f78254f82591412
Thomas Beck <T.Beck@2019.ljmu.ac.uk>
2020-08-07 20:52:07 +0100

    Added js game logic

--- js/scripts.js
+++ js/scripts.js
@@ -16,3 +16,52 @@ function Option(name, symbol, beats){
     this.symbol = symbol;
     this.beats = beats;
 }
+
+selectionButtons.forEach(selectionButton => {
+    selectionButton.addEventListener('click', e => {
+        const selectionName = selectionButton.dataset.selection
+        const selection = optionsArray.find(selection => selection.name === selectionName)
+        makeSelection(selection)
+    })
+})
+
+function makeSelection(selection) {
+    const websiteSelection = randomSelection()
+    const userWon = isWinner(selection, websiteSelection)
+    const websiteWon = isWinner(websiteSelection, selection)
+
+    if (userWon) incrementScore(userScoreSpan)
+    if (websiteWon) incrementScore(websiteScoreSpan)
+
+    displayGameResult(selection, websiteSelection, userWon, websiteWon)
+}
+
+function displayGameResult(userSelection, websiteSelection, userWon, websiteWon) {
+    displayGameChoice(userSelection, userChoiceSpan)
+    displayGameChoice(websiteSelection, websiteChoiceSpan)
+
+    displayResultBorder(userWon, "user-box")
+    displayResultBorder(websiteWon, "website-box")
+}
+
+function randomSelection() {
+    const index = Math.floor(Math.random() * optionsArray.length)
+    return optionsArray[index]
+}
+
+function isWinner(selection, opponentSelection) {
+    return selection.beats === opponentSelection.name
+}
+
+function incrementScore(scoreSpan) {
+    scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1
+}
+
+function displayGameChoice(selection, symbolSpan) {
+    symbolSpan.innerText = selection.symbol
+}
+
+function displayResultBorder(gameWon, boxName){
+    const color = (gameWon) ? 'green' : 'red';
+    document.getElementById(boxName).style.borderColor = color;
+}
